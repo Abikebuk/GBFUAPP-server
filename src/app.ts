89e217919e06d0d98c0c032e2cdf89a9446bcbc6
Rettana <rettana.muon@gmail.com>
2021-02-11 04:42:59 +0100

    fix env

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,9 @@ import express from 'express';
 import CONFIG from './init/configLoader';
 import appStart from './init/appStart';
 import cors from 'cors';
+import * as dotenv from 'dotenv';
 
+dotenv.config({ path: './config.env' });
 const port = CONFIG.SERVER.dev.port;
 const app = express();
 app.use(cors());
