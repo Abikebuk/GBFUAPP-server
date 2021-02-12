f5a1ca7d9cb144d97fcd8c4c3c3f3ae0c38a926f
Rettana <rettana.muon@gmail.com>
2021-02-12 20:25:48 +0100

    remove log from raids.ts

--- src/api/raids.ts
+++ src/api/raids.ts
@@ -8,13 +8,13 @@ function raids(app: Express, route: string, stream: Readable): number {
         res.setHeader('Content-Type', 'application/json; charset=utf-8');
         res.setHeader('Access-Control-Allow-Origin', '*');
         stream.on('data', (data) => {
-            res.write(data);
-            if (counter < 500) counter++;
-            else {
+            if (counter < 500) {
+                counter++;
+                res.write(data);
+            } else {
                 counter = 0;
                 res.end('');
             }
-            console.log(counter);
         });
     });
     return 0;
